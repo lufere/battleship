5a409f816596a05158b78d3c34a66064419523d6
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-07-19 23:52:58 -0700

    Finished the ship function and its corresponding tests

--- js/battleship.js
+++ js/battleship.js
@@ -1,4 +1,15 @@
-function sum(a, b) {
-    return a + b;
+  const Ship = (inputLength) => {
+    let length;
+    if(inputLength > 4){length = 4} else if(inputLength < 1){length = 1;} else {length = inputLength;}
+    let health = [];
+    health = Array(length).fill(true);
+    const hit = (pos) =>{
+      health.splice(pos, 1, false);
+    }
+    const isSunk = () => {
+      return health.every(pos => pos == false)
+    }
+    return{length, isSunk, health, hit}
   }
-  module.exports = sum;
+
+  module.exports = Ship;
--- js/battleship.test.js
+++ js/battleship.test.js
@@ -1,5 +1,43 @@
-const sum = require('./battleship');
+const Ship = require('./battleship');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('Create ships with the correct length', () => {
+  expect(Ship(1).length).toBe(1);
 });
+
+test('Max ship length is 4', () => {
+  expect(Ship(999).length).toBe(4);
+});
+
+test('Min ship length is 1', () => {
+  expect(Ship(-999).length).toBe(1);
+});
+
+test('Create the ship health bar', () => {
+  expect(Ship(2).health).toEqual([true, true]);
+});
+
+test('Hitting the ship successfully', () => {
+  let testShip = Ship(2);
+  testShip.hit(1);
+  expect(testShip.health).toEqual([true, false]);
+});
+
+test('Ship is not sunk until every position is hit', () => {
+  let testShip = Ship(3);
+  testShip.hit(0);
+  testShip.hit(2);
+  expect(testShip.isSunk()).toBe(false);
+});
+
+test('Ship is sunk after every position is hit', () => {
+  let testShip = Ship(3);
+  testShip.hit(0);
+  testShip.hit(1);
+  testShip.hit(2);
+  expect(testShip.isSunk()).toBe(true);
+});
+
+
+
+
+
