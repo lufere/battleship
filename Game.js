2ad945157b9a00271976dd7f69d99b12631549b9
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-08-12 23:00:30 -0700

    Created a function to streamline the grid's row creation process

--- Game.js
+++ Game.js
@@ -68,170 +68,69 @@ var Grid = function (_React$Component2) {
             });
         }
     }, {
-        key: "renderRow",
-        value: function renderRow() {
-            var _this4 = this;
-
-            // for(let i = 10; i < 20; i++){
-            //     this.renderSquare(i)
-            // }
-            return React.createElement(Square, {
-                value: this.props.board[i],
-                onClick: function onClick() {
-                    return _this4.props.onClick(i);
-                }
-            });
+        key: "createrRow",
+        value: function createrRow(row) {
+            var rowArray = [];
+            for (var i = row * 10; i < 10 + row * 10; i++) {
+                rowArray.push(this.renderSquare(i));
+            }
+            return rowArray;
         }
     }, {
         key: "render",
         value: function render() {
-            return (
-                // <button>
-                //     DELETE
-                // </button>
+            return React.createElement(
+                "div",
+                null,
                 React.createElement(
                     "div",
-                    null,
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(0),
-                        this.renderSquare(1),
-                        this.renderSquare(2),
-                        this.renderSquare(3),
-                        this.renderSquare(4),
-                        this.renderSquare(5),
-                        this.renderSquare(6),
-                        this.renderSquare(7),
-                        this.renderSquare(8),
-                        this.renderSquare(9)
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(10),
-                        this.renderSquare(11),
-                        this.renderSquare(12),
-                        this.renderSquare(13),
-                        this.renderSquare(14),
-                        this.renderSquare(15),
-                        this.renderSquare(16),
-                        this.renderSquare(17),
-                        this.renderSquare(18),
-                        this.renderSquare(19)
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(20),
-                        this.renderSquare(21),
-                        this.renderSquare(22),
-                        this.renderSquare(23),
-                        this.renderSquare(24),
-                        this.renderSquare(25),
-                        this.renderSquare(26),
-                        this.renderSquare(27),
-                        this.renderSquare(28),
-                        this.renderSquare(29)
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(30),
-                        this.renderSquare(31),
-                        this.renderSquare(32),
-                        this.renderSquare(33),
-                        this.renderSquare(34),
-                        this.renderSquare(35),
-                        this.renderSquare(36),
-                        this.renderSquare(37),
-                        this.renderSquare(38),
-                        this.renderSquare(39)
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(40),
-                        this.renderSquare(41),
-                        this.renderSquare(42),
-                        this.renderSquare(43),
-                        this.renderSquare(44),
-                        this.renderSquare(45),
-                        this.renderSquare(46),
-                        this.renderSquare(47),
-                        this.renderSquare(48),
-                        this.renderSquare(49)
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(50),
-                        this.renderSquare(51),
-                        this.renderSquare(52),
-                        this.renderSquare(53),
-                        this.renderSquare(54),
-                        this.renderSquare(55),
-                        this.renderSquare(56),
-                        this.renderSquare(57),
-                        this.renderSquare(58),
-                        this.renderSquare(59)
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(60),
-                        this.renderSquare(61),
-                        this.renderSquare(62),
-                        this.renderSquare(63),
-                        this.renderSquare(64),
-                        this.renderSquare(65),
-                        this.renderSquare(66),
-                        this.renderSquare(67),
-                        this.renderSquare(68),
-                        this.renderSquare(69)
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(70),
-                        this.renderSquare(71),
-                        this.renderSquare(72),
-                        this.renderSquare(73),
-                        this.renderSquare(74),
-                        this.renderSquare(75),
-                        this.renderSquare(76),
-                        this.renderSquare(77),
-                        this.renderSquare(78),
-                        this.renderSquare(79)
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(80),
-                        this.renderSquare(81),
-                        this.renderSquare(82),
-                        this.renderSquare(83),
-                        this.renderSquare(84),
-                        this.renderSquare(85),
-                        this.renderSquare(86),
-                        this.renderSquare(87),
-                        this.renderSquare(88),
-                        this.renderSquare(89)
-                    ),
-                    React.createElement(
-                        "div",
-                        { className: "row" },
-                        this.renderSquare(90),
-                        this.renderSquare(91),
-                        this.renderSquare(92),
-                        this.renderSquare(93),
-                        this.renderSquare(94),
-                        this.renderSquare(95),
-                        this.renderSquare(96),
-                        this.renderSquare(97),
-                        this.renderSquare(98),
-                        this.renderSquare(99)
-                    )
+                    { className: "row" },
+                    this.createrRow(0)
+                ),
+                React.createElement(
+                    "div",
+                    { className: "row" },
+                    this.createrRow(1)
+                ),
+                React.createElement(
+                    "div",
+                    { className: "row" },
+                    this.createrRow(2)
+                ),
+                React.createElement(
+                    "div",
+                    { className: "row" },
+                    this.createrRow(3)
+                ),
+                React.createElement(
+                    "div",
+                    { className: "row" },
+                    this.createrRow(4)
+                ),
+                React.createElement(
+                    "div",
+                    { className: "row" },
+                    this.createrRow(5)
+                ),
+                React.createElement(
+                    "div",
+                    { className: "row" },
+                    this.createrRow(6)
+                ),
+                React.createElement(
+                    "div",
+                    { className: "row" },
+                    this.createrRow(7)
+                ),
+                React.createElement(
+                    "div",
+                    { className: "row" },
+                    this.createrRow(8)
+                ),
+                React.createElement(
+                    "div",
+                    { className: "row" },
+                    this.createrRow(9)
                 )
             );
         }
